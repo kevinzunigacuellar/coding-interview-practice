2b6e623fec7d73283b1491ddff11d98af63cb1cd
Kevin Zuniga <kzunigac@uvm.edu>
2021-03-03 18:33:21 -0800

    Leetcode problem 2

--- Add Two Numbers.py
+++ Add Two Numbers.py
@@ -1,17 +1,17 @@
-# Leetcode Problem 2:
-# Difficulty: Medium
-# Language: Python
-# Description: You are given two non-empty linked lists representing two non-negative integers. 
-# The digits are stored in reverse order, and each of their nodes contains a single digit. 
-# Add the two numbers and return the sum as a linked list.
-# You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+"""
+Leetcode Problem 2:
+Difficulty: Medium
+Language: Python
+Description: You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. 
+Add the two numbers and return the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-# Input: l1 = [2,4,3], l2 = [5,6,4]
-# Output: [7,0,8]
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+"""
 
 # Definition for singly-linked list
-
-
 class ListNode:
      def __init__(self, val=0, next=None):
          self.val = val

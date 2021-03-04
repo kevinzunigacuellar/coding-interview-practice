6fee9876b960a5cfc8146803a9b3529f4dcb06a6
Kevin Zuniga <kzunigac@uvm.edu>
2021-03-03 18:42:48 -0800

    Bubble Sort

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,13 +1,15 @@
-# Bubble sort algorithm:
-# Description: The bubble sort algorithm have two loops. The inner loop compares each item inside the array with the next item. 
-# For this reason the loop starts with a -1 to prevent going out of the array bounds. 
-# Also after each outer loop iteration the highest/lowest number (depending of the comparing sign) will be pushed to the last position.
-# We can decrease the inner loop by (-i) at the end of each outer loop iteration since there is no reason the compare the last numbers of the array that were previously ordered. 
+"""
+Bubble sort algorithm:
+Description: The bubble sort algorithm have two loops. The inner loop compares each item inside the array with the next item. 
+For this reason the loop starts with a -1 to prevent going out of the array bounds. 
+Also after each outer loop iteration the highest/lowest number (depending of the comparing sign) will be pushed to the last position.
+We can decrease the inner loop by (-i) at the end of each outer loop iteration since there is no reason the compare the last numbers of the array that were previously ordered. 
 
-# Bubble sort optimized:
-# We can optimize the time complexity O(n^2) of this algorithm by breaking the loop if the order of the array have not changed after completing the inner loop.
-# order_changed keeps track if the list has changed or not after completing the inner loop. 
-# If there is no change we will brake out of the outher loop and end the algorithm.
+Bubble sort optimized:
+We can optimize the time complexity O(n^2) of this algorithm by breaking the loop if the order of the array have not changed after completing the inner loop.
+order_changed keeps track if the list has changed or not after completing the inner loop. 
+If there is no change we will brake out of the outher loop and end the algorithm.
+"""
 
 class SortingAlgos:
     def bubble_sort(self, arr: List[int]) -> List[int]:
